ae8a2e2fa1925da317aae20436ab4ff39e7f1c81
wwc <wwcphy@terpmail.umd.edu>
2018-10-19 21:00:28 -0400

    Enable importing of stl format for 3D multi-face electrodes

--- bem/formats/stl.py
+++ bem/formats/stl.py
@@ -4,7 +4,7 @@ import struct
 import numpy as np
 
 
-def read_stl(fil, ignore_attr_length=True):
+def read_stl(fil, ignore_attr_length=True):     # wwc stl binary format
     """reads binary STL in file "fil" with Inventor style
     (color hidden in attribute length), returns array of normals, array
     of vertices and list or attributes
@@ -17,9 +17,9 @@ def read_stl(fil, ignore_attr_length=True):
     (default color if MSB is 0) COLOR=\\xbf\\xbf\\xbf\\xff in the header
     is the overall color default color.
     """
-    h = fil.read(80)
-    assert h.startswith("STLB")
-    n, = struct.unpack("< I", fil.read(4))
+    h = fil.read(80)        # wwc A ASCII header of length 80 bytes.
+    assert h.startswith("STLB")     # wwc see if it's a binary stl or raise an AssertionError.
+    n, = struct.unpack("< I", fil.read(4))      # wwc n may be number of faces in the file. 4 bytes unsigned int. "< I" google python struct.unpack and see https://docs.python.org/3.6/library/struct.html#format-strings
     if ignore_attr_length:
         dtype = np.dtype([
             ("normal", "<f4", (3,)),
@@ -51,6 +51,64 @@ def check_normals(normals, triangles):
     n /= np.sqrt(np.sum(np.square(n), axis=1))[:, None]
     assert np.allclose(n, normals, rtol=1e-3, atol=1e-10)
 
+# wwc function
+def partition_normals(normals,triangles,numbers=[],TOL=1e-6):
+    """Partition points into different planes according to the normals in 
+    one electrode, for 3D/2D meshing (Shewchuk's triangle C code is 2D meshing).
+    Return points_numbers which has following format
+
+    [(plane1_points,serial_numbers1),(plane2_points,serial_numbers2),...]
+
+    plane_points -- array([ [x1,y1,z1],[x2,y2,z2],... [xn,yn,zn] ])
+    serial_numbers -- array([ [0,1,2],[3,4,5],... [n-3,n-2,n-1] ])
+
+    TOL: normal deviation tolerances
+    """
+    nm_unique = [normals[0]]      # normals[0] as first nm  wwc
+    points = [[]]    # Each sublist [] represents a plane  wwc
+    for nm, tr in zip(normals, triangles):
+        add_plane = True
+        # Search existing normals in nm_unique, in reversed order (points in the same plane are often ajacent.)  wwc
+        for ith in range(len(nm_unique)-1,-1,-1):   # ith normal -- ith plane  wwc
+            if np.linalg.norm(nm-nm_unique[ith]) < TOL:
+                # points[ith] plane -- [array([x1,y1,z1]),array([x2,y2,z2]), ...]  wwc
+                points[ith].extend(tr)
+                add_plane = False
+                break
+        if add_plane:   # All normals don't coincide with the new nm, record it.  wwc
+            nm_unique.append(nm)
+            points.append([])
+            points[-1].extend(tr)
+    points_numbers = []
+    for plane in points:
+        plane_points = np.ascontiguousarray(plane,dtype=np.double)
+        i = np.arange(0,plane_points.shape[0],3)    # shape[0] is the number of points in the plane.  wwc
+        serial_numbers = np.c_[i, i+1, i+2].astype(np.intc)
+        points_numbers.append( (plane_points,serial_numbers) )
+    return points_numbers, np.array(nm_unique,dtype=np.double)
+
+# wwc function. Copied from cpy.py. This function can split normals merely by 3 points coordinates of triangles, 
+# though we alreadly have normals imported from stl file. So use above partition_normals() is OK.  wwc
+def split_by_normal(stlmesh):
+    """split curved faces into one face per triangle (aka split by
+    normal, planarize). in place"""
+    for name, faces in stlmesh.iteritems():    # An iterator  wwc
+        new_faces = []
+        # faces = [(points,triangles)], so points, triangles = (points, triangles)  wwc
+        for points, triangles in faces:
+            x = points[triangles, :]    # triangles are the serial number of points.  wwc
+            # For all triangle points x0,x1,x2, get the cross product of vector x1-x0, x2-x0, the normal vectors.  wwc
+            normals = np.cross(x[:, 1]-x[:, 0], x[:, 2]-x[:, 0])
+            normals /= np.sqrt(np.sum(np.square(normals), axis=1))[:, None]
+            if np.allclose(normals, normals[0][None, :]):
+                new_faces.append((points, triangles))
+            else:
+                for triangle in triangles:
+                    new_faces.append((points[triangle, :],
+                        np.arange(3, dtype=np.intc).reshape((1, 3))))
+        stlmesh[name] = new_faces
+    return stlmesh
+
 
 def stl_to_mesh(normals, triangles, attributes, scale=None, rename=None):
     """generates a {name: [(points, triangles)]} mesh from the
@@ -72,27 +130,42 @@ def stl_to_mesh(normals, triangles, attributes, scale=None, rename=None):
     color_numer, else if the color is not found in rename,
     the electrode is dropped.
     """
-    d = OrderedDict()
-    for a, v in zip(attributes, triangles):
-        d.setdefault(a, []).append(v)
-    o = OrderedDict()
-    for a, v in d.iteritems():
-        v = np.array(v)
-        i = np.arange(0, v.shape[0]*3, 3)
-        points = v.reshape(-1, 3).astype(np.double)
-        if scale:
-            points /= scale
-        triangles = np.c_[i, i+1, i+2].astype(np.intc)
+    d = OrderedDict()    # d is an internediate dict which will pass the items to o.  wwc
+    for a, nm, tr in zip(attributes, normals, triangles):
+        d.setdefault(a, [[],[]])[0].append(nm) 
+        d[a][1].append(tr)
+        # d[a] = [nms, trs], nms = [array([x,y,z]),...] trs = [ array([[x1,y1,z1],[x2,y2,z2],[x3,y3,z3]]),...]  wwc
+    o = OrderedDict()    # o stores the {name: [(points, triangles)]}  wwc
+    for a, nm_tr in d.iteritems():    # a is stl color number.  wwc
+        nms, trs = np.array(nm_tr[0]), np.array(nm_tr[1])
+        if scale:    # scale a dimensionless number. For example, scale = 40.  wwc
+            # points /= scale
+            trs /= scale    # nms are unit vectors.  wwc
+        partition = False
         if rename:
             if a not in rename:
+                # If you don't know the number of color a, this prints it for you to rename.  wwc
                 print "dropping", a
                 continue
             else:
-                n = rename[a]
+                n = rename[a]    # If you have provided a rename for a, this renames it.
+                partition = True
         else:
             n = "stl_%i" % a
-        o[n] = [(np.ascontiguousarray(points),
-            np.ascontiguousarray(triangles))]
+        if partition:    # Designed for 3D multiplane partition.  wwc  
+            o[n], planes = partition_normals(nms, trs)
+            print "%i planes in electrode %s"%(len(planes),n)
+            print "Normals vectors are:\n", planes
+        else:    # Robert's original 2D version.  wwc
+            i = np.arange(0, trs.shape[0]*3, 3)
+            points = trs.reshape(-1, 3).astype(np.double)    # reshape puts all points together, not distinguish different triangles.  wwc
+            triangles = np.c_[i, i+1, i+2].astype(np.intc)    # "triangles" here is actually serial number of points.  wwc
+            # Note there is an additional list [ ] contains the tuple (points, triangles).  wwc
+            # Robert might leave this as a future interface for 3D meshing.  wwc
+            o[n] = [(np.ascontiguousarray(points),    # n = o.keys(), is "DC1","RF"..., the rename.  wwc
+                np.ascontiguousarray(triangles))]     # triangles are the serial number of points.  wwc
+        # o[n] = [(np.ascontiguousarray(points),
+        #         np.ascontiguousarray(triangles))]
     return o
 
 
